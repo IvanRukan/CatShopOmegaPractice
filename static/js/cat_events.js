2c3221c0136661d9196c8f5246acd7dc890a585b
Ivan Rukan <07vanek@gmail.com>
2024-07-11 16:48:30 +0300

    implemented edit feature for admin users

--- static/js/cat_events.js
+++ static/js/cat_events.js
@@ -16,5 +16,24 @@ function choose_cat() {
     });
 }
 
+
+function edit_cat() {
+    $.ajax({
+        type: 'GET',
+        url: '/edit_chosen_cat',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: {
+            'id': document.getElementById('cat_id').innerHTML,
+        },
+        success: function (response) {
+            console.log(response['ID'])
+            window.location.href = '/edit_chosen_cat/' + response['ID']
+        }
+    });
+}
+
+
 document.getElementById('choose_cat').addEventListener('click', choose_cat)
+document.getElementById('edit_cat').addEventListener('click', edit_cat)
 
